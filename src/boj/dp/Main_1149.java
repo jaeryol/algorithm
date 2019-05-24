a90f9e81610ffdfa0e4609b2191d4d0f22a8ab4e
Jaeryong Im <hackerrs.im@gmail.com>
2019-05-24 18:06:08 +0900

    1149 문제 풀이
    - RGB 거리(DP)

---
+++ src/boj/dp/Main_1149.java
@@ -0,0 +1,32 @@
+package boj.dp;
+
+/*
+    링크 : https://www.acmicpc.net/problem/1149
+    유형 : 다이나믹 프로그래밍
+    문제 이름 : RGB 거리
+ */
+
+import java.util.Scanner;
+
+public class Main_1149 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] a = new int[n + 1][3];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] d = new int[n + 1][3];
+        for (int i = 1; i <= n; i++) {
+            d[i][0] = Math.min(d[i - 1][1], d[i - 1][2]) + a[i][0];
+            d[i][1] = Math.min(d[i - 1][0], d[i - 1][2]) + a[i][1];
+            d[i][2] = Math.min(d[i - 1][0], d[i - 1][1]) + a[i][2];
+        }
+
+        System.out.println(Math.min(Math.min(d[n][0], d[n][1]), d[n][2]));
+    }
+}
